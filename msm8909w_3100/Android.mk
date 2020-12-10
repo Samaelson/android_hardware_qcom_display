7cd831a2adc91247affdc812a60f9fe6b645e9dd
Samaelson <andreas.as.schramm@gmx.de>
2020-12-10 08:50:46 +0100

    display: add TARGET_PROVIDES_LIBLIGHT

--- msm8909w_3100/Android.mk
+++ msm8909w_3100/Android.mk
@@ -2,7 +2,7 @@ sdm-libs := sdm/libs
 display-hals := include libqservice libqdutils $(sdm-libs)/utils $(sdm-libs)/core
 
 ifneq ($(TARGET_IS_HEADLESS), true)
-    display-hals += libcopybit liblight libmemtrack hdmi_cec \
+    display-hals += libcopybit libmemtrack hdmi_cec \
                     $(sdm-libs)/hwc $(sdm-libs)/hwc2 gpu_tonemapper libdrmutils libdisplayconfig
 endif
 
@@ -10,6 +10,10 @@ ifneq ($(TARGET_USES_GRALLOC1), true)
     display-hals += libgralloc
 else
     display-hals += libgralloc1
+endif
+
+ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+    display-hals += liblight
 endif
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
