654b79ce14b6aae9fdb45cff2e09b030d3cfcc99
Samaelson <andreas.as.schramm@gmx.de>
2020-12-10 07:48:37 +0100

    msm8084: Add display headers lib

--- msm8084/Android.mk
+++ msm8084/Android.mk
@@ -1,6 +1,10 @@
-display-hals := libgralloc libcopybit liblight libvirtual
+display-hals := libgralloc libcopybit libvirtual
 display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
 display-hals += libmemtrack
+ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+    display-hals += liblight
+endif
+
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     include $(call all-named-subdir-makefiles,$(display-hals))
 else
@@ -8,3 +12,16 @@ ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
 endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := display_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := \
+    $(display_top)/libcopybit \
+    $(display_top)/libexternal \
+    $(display_top)/libgralloc \
+    $(display_top)/libhwcomposer \
+    $(display_top)/liboverlay \
+    $(display_top)/libqdutils \
+    $(display_top)/libqservice \
+    $(display_top)/libvirtual
+include $(BUILD_HEADER_LIBRARY)
